bad6293cab7c11f75d7cad002443d0ecd7368328
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-10-13 20:42:43 -0300

    feat: keeps server running undefinitely.

--- server.c
+++ server.c
@@ -6,13 +6,14 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 17:46:35 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/10/07 17:57:29 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/10/13 20:40:08 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <sys/types.h>
 #include <unistd.h>
+#include "libft/libft.h"
 
 int	server(void)
 {
@@ -20,11 +21,15 @@ int	server(void)
 
 	pid = 0;
 	pid = getpid();
+	ft_printf("%d", pid);
+	while(1)
+	{
+		sleep(1);
+	}
 	return (pid);
 }
 
-#include <stdio.h>
 int	main(void)
 {
-	printf("%d", server());
+	server();
 }
