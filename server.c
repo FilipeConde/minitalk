c72c38b22a84ec27bf187262608a884b155039fd
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-10-13 22:39:05 -0300

    chore: sets response for signal_hendler().

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 17:46:35 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/10/13 22:19:10 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/10/13 22:38:24 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,10 @@
 
 void	signal_handler(int sigchannel, siginfo_t *info, void *context)
 {
+	(void)context;
 	ft_printf("SIG HANDLER ACCESSED: val => %d\n", sigchannel);
-	// put some response for client when received
+	kill(info->si_pid, 1);
 }
-
-// int	server(void)
-// {
-// 	pid_t				pid;
-// 	struct sigaction	sa;
-
-// 	sa.sa_handler = signal_handler;
-// 	sigemptyset(&sa.sa_mask); // review aproach
-// 	sa.sa_flags = SA_SIGINFO; // review aproach
-// 	// pid = 0;
-// 	pid = getpid();
-// 	ft_printf("Server PID: %d\n", pid);
-
-// 	while(1)
-// 	{
-// 		sleep(1);
-// 	}
-// 	return (0);
-// }
 
 int	main(void)
 {
@@ -61,6 +43,7 @@ int	main(void)
 	while(1)
 	{
 		pause();
+		// or usleep(100);
 	}
 	return (0);
 }
