f85f5b47bb650c892262b76360f9ed5ca0fcbcee
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-10-13 21:56:40 -0300

    chore: starts signal listener at server.

--- server.c
+++ server.c
@@ -6,22 +6,32 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 17:46:35 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/10/13 20:40:08 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/10/13 21:51:38 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# define _POSIX_C_SOURCE 200809L
 #include "libft/libft.h"
-#include <sys/types.h>
 #include <unistd.h>
-#include "libft/libft.h"
+#include <signal.h>
+
+void	signal_handler(int sigchannel, siginfo_t *info)
+{
+	ft_printf("SIG HANDLER ACCESSED: val => %d\n", sigchannel);
+}
 
 int	server(void)
 {
-	pid_t	pid;
+	pid_t				pid;
+	struct sigaction	sa;
 
+	sa.sa_handler = signal_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
 	pid = 0;
 	pid = getpid();
 	ft_printf("%d", pid);
+
 	while(1)
 	{
 		sleep(1);
